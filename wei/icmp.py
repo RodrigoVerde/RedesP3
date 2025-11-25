667ad911cc11b5008ee0f49675bb2617faccf1f3
Wei Wei Zhu <weiw.zhu@estudiante.uam.es>
2025-11-25 11:31:43 +0100

    Add files via upload

---
+++ wei/icmp.py
@@ -0,0 +1,163 @@
+'''
+	icmp.py
+	
+	Funciones necesarias para implementar el nivel ICMP
+	Autor: Javier Ramos <javier.ramos@uam.es>
+	2022 EPS-UAM
+'''
+from ip import *
+from threading import Lock
+import struct
+
+import binascii
+
+ICMP_PROTO = 1
+
+ICMP_ECHO_REQUEST_TYPE = 8
+ICMP_ECHO_REPLY_TYPE = 0
+CODE = 0
+ID = 10
+SEQ_NUM = 0
+
+timeLock = Lock()
+icmp_send_times = {}
+
+def process_ICMP_message(us,header,data,srcIp):
+	'''
+		Nombre: process_ICMP_message
+		Descripción: Esta función procesa un mensaje ICMP. Esta función se ejecutará por cada datagrama IP que contenga
+		un 1 en el campo protocolo de IP
+		Esta función debe realizar, al menos, las siguientes tareas:
+			-Calcular el checksum de ICMP y comprobar si es correcto:
+			-Extraer campos tipo y código de la cabecera ICMP
+			-Loggear (con logging.debug) el valor de tipo y código
+			-Si el tipo es ICMP_ECHO_REQUEST_TYPE:
+				-Generar un mensaje de tipo ICMP_ECHO_REPLY como respuesta. Este mensaje debe contener
+				los datos recibidos en el ECHO_REQUEST. Es decir, "rebotamos" los datos que nos llegan.
+				-Enviar el mensaje usando la función sendICMPMessage
+			-Si el tipo es ICMP_ECHO_REPLY_TYPE:
+				-Extraer del diccionario icmp_send_times el valor de tiempo de envío usando como clave los campos srcIP e icmp_id e icmp_seqnum
+				contenidos en el mensaje ICMP. Restar el tiempo de envio extraído con el tiempo de recepción (contenido en la estructura pcap_pkthdr)
+				-Se debe proteger el acceso al diccionario de tiempos usando la variable timeLock
+				-Mostrar por pantalla la resta. Este valor será una estimación del RTT
+			-Si es otro tipo:
+				-No hacer nada
+
+		Argumentos:
+			-us: son los datos de usuarios pasados por pcap_loop (en nuestro caso este valor será siempre None)
+			-header: estructura pcap_pkthdr que contiene los campos len, caplen y ts.
+			-data: array de bytes con el conenido del mensaje ICMP
+			-srcIP: dirección IP que ha enviado el datagrama actual.
+		Retorno: Ninguno
+		  
+	'''
+	global ICMP_ECHO_REPLY_TYPE, ICMP_ECHO_REQUEST_TYPE, CODE, timeLock, icmp_send_times
+	
+	#Comprobamos checksum
+	if (chksum(data) != 0):
+		return
+	
+	#Extraemos campos
+	icmpType = data[0]
+	code = data[1]
+	icmpId = int.from_bytes(data[4:6], 'big')
+	seqNum = int.from_bytes(data[6:8], 'big')
+	payload = data[8:]
+	
+	#Loggueamos
+	logging.debug(f'Type: {icmpType}\tCode: {code}')
+	
+	#Procesamos paquete
+	if (icmpType == ICMP_ECHO_REQUEST_TYPE and code == CODE):
+		sendICMPMessage(payload, ICMP_ECHO_REPLY_TYPE, CODE, icmpId, seqNum, int.from_bytes(srcIp, 'big'))
+	elif (icmpType == ICMP_ECHO_REPLY_TYPE and code == CODE):
+		with timeLock:
+			if (int.from_bytes(srcIp, 'big') + icmpId + seqNum not in icmp_send_times):
+				return
+			ti = icmp_send_times[int.from_bytes(srcIp, 'big') + icmpId + seqNum]
+		
+		rtt = header.ts.tv_sec + (header.ts.tv_usec / 1000000) - ti
+		logging.info(f'RTT: {rtt}')
+	
+	return
+	
+
+def sendICMPMessage(data,type,code,icmp_id,icmp_seqnum,dstIP,minSize = 0,padding = b'0'):
+	'''
+		Nombre: sendICMPMessage
+		Descripción: Esta función construye un mensaje ICMP y lo envía.
+		Esta función debe realizar, al menos, las siguientes tareas:
+			-Si el campo type es ICMP_ECHO_REQUEST_TYPE o ICMP_ECHO_REPLY_TYPE:
+				-Construir la cabecera ICMP
+				-Añadir los datos al mensaje ICMP
+				-Calcular el checksum y añadirlo al mensaje donde corresponda
+				-Si type es ICMP_ECHO_REQUEST_TYPE
+					-Guardar el tiempo de envío (llamando a time.time()) en el diccionario icmp_send_times
+					usando como clave el valor de dstIp+icmp_id+icmp_seqnum
+					-Se debe proteger al acceso al diccionario usando la variable timeLock
+
+				-Llamar a sendIPDatagram para enviar el mensaje ICMP
+				
+			-Si no:
+				-Tipo no soportado. Se devuelve False
+
+		Argumentos:
+			-data: array de bytes con los datos a incluir como payload en el mensaje ICMP
+			-type: valor del campo tipo de ICMP
+			-code: valor del campo code de ICMP 
+			-icmp_id: entero que contiene el valor del campo ID de ICMP a enviar
+			-icmp_seqnum: entero que contiene el valor del campo Seqnum de ICMP a enviar
+			-dstIP: entero de 32 bits con la IP destino del mensaje ICMP
+		Retorno: True o False en función de si se ha enviado el mensaje correctamente o no
+		  
+	'''
+	global ICMP_ECHO_REQUEST_TYPE, ICMP_ECHO_REPLY_TYPE, timeLock, icmp_send_times, ICMP_PROTO
+	
+	#Control de errores: Tipo no soportado
+	if (type not in [ICMP_ECHO_REPLY_TYPE, ICMP_ECHO_REQUEST_TYPE]):
+		return
+	
+	icmp_message = bytearray()
+
+	#Construimos paquete
+	icmp_message[0:1] = (type).to_bytes(1, 'big')
+	icmp_message[1:2] = (code).to_bytes(1, 'big')
+	icmp_message[2:4] = bytes([0x00, 0x00])
+	icmp_message[4:6] = icmp_id.to_bytes(2, 'big')
+	icmp_message[6:8] = icmp_seqnum.to_bytes(2, 'big')
+	icmp_message[8:] = data
+	
+	#Añadimos relleno para que el paquete sea del tamaño pedido
+	if (len(icmp_message) < minSize):
+		icmp_message[len(ismp_message):] = bytearray(minSize - len(ismp_message))
+	
+	#Checksum: Numero de bytes pares
+	if (len(icmp_message) % 2 != 0):
+		icmp_message.append(0)
+	
+	#Checksum: Calculo
+	icmp_message[2:4] = chksum(icmp_message).to_bytes(2, 'little')
+	
+	#Si es Echo Request
+	if (type == ICMP_ECHO_REQUEST_TYPE):
+		with timeLock:
+			icmp_send_times[dstIP + icmp_id + icmp_seqnum] = time.time()
+			
+	sendIPDatagram(dstIP, icmp_message, ICMP_PROTO)
+
+
+def initICMP():
+	'''
+		Nombre: initICMP
+		Descripción: Esta función inicializa el nivel ICMP
+		Esta función debe realizar, al menos, las siguientes tareas:
+			-Registrar (llamando a registerIPProtocol) la función process_ICMP_message con el valor de protocolo 1
+
+		Argumentos:
+			-Ninguno
+		Retorno: Ninguno
+		  
+	'''
+	global ICMP_PROTO
+	
+	registerIPProtocol(process_ICMP_message, ICMP_PROTO)
---
+++ wei/ip.py
@@ -0,0 +1,345 @@
+'''
+	ip.py
+	
+	Funciones necesarias para implementar el nivel IP
+	Autor: Javier Ramos <javier.ramos@uam.es>
+	2022 EPS-UAM
+'''
+from ethernet import *
+from arp import *
+from fcntl import ioctl
+import subprocess
+import math
+
+SIOCGIFMTU = 0x8921
+SIOCGIFNETMASK = 0x891b
+
+#Diccionario de protocolos. Las claves con los valores numéricos de protocolos de nivel superior a IP
+#por ejemplo (1, 6 o 17) y los valores son los nombres de las funciones de callback a ejecutar.
+protocols={}
+
+#Tamaño mínimo de la cabecera IP
+IP_MIN_HLEN = 20
+#Tamaño máximo de la cabecera IP
+IP_MAX_HLEN = 60
+
+#Constantes
+VERSION = 0x04
+TYPE_OF_SERVICE = 0x1
+NUM_PAREJA = 10
+TIME_TO_LIVE = 65
+
+myIP = None
+MTU = None
+netmask = None
+defaultGW = None
+
+ETHERTYPE = int.from_bytes(bytes([0x08,0x00]), 'big')
+
+def chksum(msg):
+	'''
+		Nombre: chksum
+		Descripción: Esta función calcula el checksum IP sobre unos datos de entrada dados (msg)
+		Argumentos:
+			-msg: array de bytes con el contenido sobre el que se calculará el checksum
+		Retorno: Entero de 16 bits con el resultado del checksum en ORDEN DE RED
+	'''
+	s = 0
+	y = 0xa29f
+	for i in range(0, len(msg), 2):
+		if (i+1) < len(msg):
+			a = msg[i] 
+			b = msg[i+1]
+			s = s + (a+(b << 8))
+		elif (i+1)==len(msg):
+			s += msg[i]
+		else:
+			raise 'Error calculando el checksum'
+	y = y & 0x00ff
+	s = s + (s >> 16)
+	s = ~s & 0xffff
+
+	return s
+
+def getMTU(interface):
+	'''
+		Nombre: getMTU
+		Descripción: Esta función obteiene la MTU para un interfaz dada
+		Argumentos:
+			-interface: cadena con el nombre la interfaz sobre la que consultar la MTU
+		Retorno: Entero con el valor de la MTU para la interfaz especificada
+	'''
+	s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
+	ifr = struct.pack('16sH', interface.encode("utf-8"), 0)
+	mtu = struct.unpack('16sH', ioctl(s,SIOCGIFMTU, ifr))[1]
+   
+	s.close()
+   
+	return mtu
+   
+def getNetmask(interface):
+	'''
+		Nombre: getNetmask
+		Descripción: Esta función obteiene la máscara de red asignada a una interfaz 
+		Argumentos:
+			-interface: cadena con el nombre la interfaz sobre la que consultar la máscara
+		Retorno: Entero de 32 bits con el valor de la máscara de red
+	'''
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	ip = fcntl.ioctl(
+		s.fileno(),
+	   SIOCGIFNETMASK,
+		struct.pack('256s', (interface[:15].encode('utf-8')))
+	)[20:24]
+	s.close()
+	return struct.unpack('!I',ip)[0]
+
+
+def getDefaultGW(interface):
+	'''
+		Nombre: getDefaultGW
+		Descripción: Esta función obteiene el gateway por defecto para una interfaz dada
+		Argumentos:
+			-interface: cadena con el nombre la interfaz sobre la que consultar el gateway
+		Retorno: Entero de 32 bits con la IP del gateway
+	'''
+	p = subprocess.Popen(['ip r | grep default | awk \'{print $3}\''], stdout=subprocess.PIPE, shell=True)
+	dfw = p.stdout.read().decode('utf-8')
+	print(dfw)
+	return struct.unpack('!I',socket.inet_aton(dfw))[0]
+	
+
+def process_IP_datagram(us,header,data,srcMac):
+	'''
+		Nombre: process_IP_datagram
+		Descripción: Esta función procesa datagramas IP recibidos.
+			Se ejecuta una vez por cada trama Ethernet recibida con Ethertype 0x0800
+			Esta función debe realizar, al menos, las siguientes tareas:
+				-Extraer los campos de la cabecera IP (includa la longitud de la cabecera)
+				-Calcular el checksum y comprobar que es correcto					
+				-Analizar los bits de de MF y el offset. Si el offset tiene un valor != 0 dejar de procesar el datagrama (no vamos a reensamblar)
+				-Loggear (usando logging.debug) el valor de los siguientes campos:
+					-Longitud de la cabecera IP
+					-IPID
+					-TTL
+					-Valor de las banderas DF y MF
+					-Valor de offset
+					-IP origen y destino
+					-Protocolo
+				-Comprobar si tenemos registrada una función de callback de nivel superior consultando el diccionario protocols y usando como
+				clave el valor del campo protocolo del datagrama IP.
+					-En caso de que haya una función de nivel superior registrada, debe llamarse a dicha funciñón 
+					pasando los datos (payload) contenidos en el datagrama IP.
+		
+		Argumentos:
+			-us: Datos de usuario pasados desde la llamada de pcap_loop. En nuestro caso será None
+			-header: cabecera pcap_pktheader
+			-data: array de bytes con el contenido del datagrama IP
+			-srcMac: MAC origen de la trama Ethernet que se ha recibido
+		Retorno: Ninguno
+	'''
+	global VERSION, IP_MIN_HLEN, IP_MAX_HLEN, protocols
+	
+	#Extraemos la informacion de la cabecera
+	version = (data[0] & 0xf0) >> 4
+	header_len = (data[0] & 0x0f) 
+	type_of_service = data[1]
+	total_len = data[2:4]
+	identi = data[4:6]
+	df = (data[6] & 0x4) >> 2
+	mf = (data[6] & 0x2) >> 1
+	offset = (int.from_bytes(data[6:8], 'big') & 0x1fff)
+	time_to_live = data[8]
+	protocol = data[9]
+	checksum = data[10:12]
+	source_IP = data[12:16]
+	dest_IP = data[16:20]
+	
+	#Control de errores: Verison
+	if (version != VERSION):
+		return
+	
+	#Control de errores: Header length correcto
+	if (header_len*4 < IP_MIN_HLEN or header_len*4 > IP_MAX_HLEN):
+		return
+	
+	#Control de errores: checksum
+	if (chksum(data[0:header_len*4]) != 0):
+		return
+		
+	#Control de errores: Fragmentacion
+	if (offset != 0):
+		return
+		
+	#Logging
+	a = int.from_bytes(identi, 'big')
+	b = str(source_IP[0]) + '.' + str(source_IP[1]) + '.' + str(source_IP[2]) + '.' + str(source_IP[3])
+	c = str(dest_IP[0]) + '.' + str(dest_IP[1]) + '.' + str(dest_IP[2]) + '.' + str(dest_IP[3])
+	logging.debug(f'Header length: {header_len*4}\nIPID: {a}\nTTL: {time_to_live}\nDF: {df}\nMF: {mf}\nValor de offset: {offset}\nIP origen: {b}\nIP destino: {c}\nProtocolo: {protocol}')
+	
+	#Control de errores: Protocolo superior
+	if (protocol not in protocols):
+		return
+	
+	payload = data[header_len*4:]
+	
+	protocols[protocol](us, header, payload, source_IP)
+
+
+def registerIPProtocol(callback,protocol):
+	'''
+		Nombre: registerIPProtocol
+		Descripción: Esta función recibirá el nombre de una función y su valor de protocolo IP asociado y añadirá en la tabla 
+			(diccionario) de protocolos de nivel superior dicha asociación. 
+			Este mecanismo nos permite saber a qué función de nivel superior debemos llamar al recibir un datagrama IP  con un 
+			determinado valor del campo protocolo (por ejemplo TCP o UDP).
+			Por ejemplo, podemos registrar una función llamada process_UDP_datagram asociada al valor de protocolo 17 y otra 
+			llamada process_ICMP_message asocaida al valor de protocolo 1. 
+		Argumentos:
+			-callback_fun: función de callback a ejecutar cuando se reciba el protocolo especificado. 
+				La función que se pase como argumento debe tener el siguiente prototipo: funcion(us,header,data,srcIp):
+				Dónde:
+					-us: son los datos de usuarios pasados por pcap_loop (en nuestro caso este valor será siempre None)
+					-header: estructura pcap_pkthdr que contiene los campos len, caplen y ts.
+					-data: payload del datagrama IP. Es decir, la cabecera IP NUNCA se pasa hacia arriba.
+					-srcIP: dirección IP que ha enviado el datagrama actual.
+				La función no retornará nada. Si un datagrama se quiere descartar basta con hacer un return sin valor y dejará de procesarse.
+			-protocol: valor del campo protocolo de IP para el cuál se quiere registrar una función de callback.
+		Retorno: Ninguno 
+	'''
+	global protocols
+	
+	protocols[protocol] = callback
+	
+
+def initIP(interface,opts=None):
+	global myIP, MTU, netmask, defaultGW, ipOpts, ETHERTYPE, IPID
+	'''
+		Nombre: initIP
+		Descripción: Esta función inicializará el nivel IP. Esta función debe realizar, al menos, las siguientes tareas:
+			-Llamar a initARP para inicializar el nivel ARP
+			-Obtener (llamando a las funciones correspondientes) y almacenar en variables globales los siguientes datos:
+				-IP propia
+				-MTU
+				-Máscara de red (netmask)
+				-Gateway por defecto
+			-Almacenar el valor de opts en la variable global ipOpts
+			-Registrar a nivel Ethernet (llamando a registerCallback) la función process_IP_datagram con el Ethertype 0x0800
+			-Inicializar el valor de IPID con el número de pareja
+		Argumentos:
+			-interface: cadena de texto con el nombre de la interfaz sobre la que inicializar ip
+			-opts: array de bytes con las opciones a nivel IP a incluir en los datagramas o None si no hay opciones a añadir
+		Retorno: True o False en función de si se ha inicializado el nivel o no
+	'''
+	if (initARP(interface) < 0):
+		return False
+	
+	myIP = getIP(interface)
+	MTU = getMTU(interface)
+	netmask = getNetmask(interface)
+	defaultGW = getDefaultGW(interface).to_bytes(6, 'big')
+	
+	#Control de errores: Opciones
+	if (opts is not None and len(opts) > IP_MAX_HLEN - 20):
+		return
+	
+	while (opts is not None and len(opts) % 4 != 0):
+		opts.append(0)
+	
+	ipOpts = opts
+	
+	registerEthCallback(process_IP_datagram, ETHERTYPE)
+	
+	IPID = NUM_PAREJA
+	
+	return True
+	
+
+def sendIPDatagram(dstIP,data,protocol):
+	global IPID, ETHERTYPE, netmask, myIP, ipOpts
+	'''
+		Nombre: sendIPDatagram
+		Descripción: Esta función construye un datagrama IP y lo envía. En caso de que los datos a enviar sean muy grandes la función
+		debe generar y enviar el número de fragmentos IP que sean necesarios.
+		Esta función debe realizar, al menos, las siguientes tareas:
+			-Determinar si se debe fragmentar o no y calcular el número de fragmentos
+			-Para cada datagrama o fragmento:
+				-Construir la cabecera IP con los valores que corresponda.Incluir opciones en caso de que ipOpts sea distinto de None
+				-Calcular el checksum sobre la cabecera y añadirlo a la cabecera
+				-Añadir los datos a la cabecera IP
+				-En el caso de que sea un fragmento ajustar los valores de los campos MF y offset de manera adecuada
+				-Enviar el datagrama o fragmento llamando a sendEthernetFrame. Para determinar la dirección MAC de destino
+				al enviar los datagramas se debe hacer unso de la máscara de red:				  
+			-Para cada datagrama (no fragmento):
+				-Incrementar la variable IPID en 1.
+		Argumentos:
+			-dstIP: entero de 32 bits con la IP destino del datagrama 
+			-data: array de bytes con los datos a incluir como payload en el datagrama
+			-protocol: valor numérico del campo IP protocolo que indica el protocolo de nivel superior de los datos
+			contenidos en el payload. Por ejemplo 1, 6 o 17.
+		Retorno: True o False en función de si se ha enviado el datagrama correctamente o no
+		  
+	'''
+	#Si los datos son luy grandes (más que IP permite) descartamos
+	if (ipOpts is not None):
+		if (len(data) > (65535 - 20 - len(ipOpts))):
+			return
+	else:
+		if (len(data) > (65535 - 20)):
+			return
+	
+	datagram = bytearray()
+	payload_size = None
+	
+	#Calculo fragmentos
+	if (ipOpts is None):
+		payload_size = math.floor((MTU - IP_MIN_HLEN) / 8) * 8
+	else:
+		payload_size = math.floor((MTU - IP_MIN_HLEN - len(ipOpts)) / 8) * 8
+	
+	num_frag = math.ceil(len(data) / payload_size)
+	
+	#Calculamos la direccion MAC destino
+	dstMac = defaultGW
+	if ((netmask & dstIP) == (netmask & myIP)):
+		dstMac = ARPResolution(dstIP)
+	
+	for i in range(num_frag):
+		#Creamos los fragmentos
+		mf = 0 if (i == num_frag-1) else 1
+		datagram_size = len(data) if (num_frag == 1) else payload_size if (i != num_frag-1) else (len(data) - payload_size*i)
+		datagram[0:] = createIPHeader(IPID, mf, payload_size*i, protocol, dstIP, datagram_size)
+		datagram[len(datagram):] = data[payload_size*i: payload_size*(i+1)]
+		
+		#Enviamos fragmento
+		if (sendEthernetFrame(datagram, len(datagram), ETHERTYPE, dstMac) < 0):
+			return False
+
+	IPID += 1 
+	
+	return True
+	
+
+def createIPHeader(ipid, mf, offset, protocol, dstIP, payload_size):
+	global ipOpts, VERSION, TYPE_OF_SERVICE, TIME_TO_LIVE, myIP
+	header = bytearray()
+	
+	header[0:1] = (VERSION << 4).to_bytes(1, 'big')
+	header[1:2] = (TYPE_OF_SERVICE).to_bytes(1, 'big')
+	header[2:4] = bytes([0x00, 0x00])
+	header[4:6] = ipid.to_bytes(2, 'big')
+	header[6:8] = ((mf << 13) + math.floor(offset/8)).to_bytes(2, 'big')
+	header[8:9] = (TIME_TO_LIVE).to_bytes(1, 'big')
+	header[9:10] = (protocol).to_bytes(1, 'big')
+	header[10:12] = bytes([0x00, 0x00])
+	header[12:16] = myIP.to_bytes(4, 'big')
+	header[16:20] = dstIP.to_bytes(4, 'big')
+	if (ipOpts is not None):
+		header[20:] = ipOpts
+	
+	#Corregimos algunos campos que necesitaban de la cabecera completa
+	header[0:1] = (header[0] + math.floor(len(header) / 4)).to_bytes(1, 'big')
+	header[2:4] = (len(header) + payload_size).to_bytes(2, 'big')
+	header[10:12] = chksum(header).to_bytes(2, 'little')
+	
+	return header
